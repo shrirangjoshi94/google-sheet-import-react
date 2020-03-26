a280127b86ca295c0aac59b8c1de08a10b3990c2
Shrirang <shrirangjoshi94@gmail.com>
2020-03-27 00:28:21 +0530

    Initial import.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Tabletop from 'tabletop';
+import { Table } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    Tabletop.init({
+      key: '1Gu_BOKVF4SG8CR32gCV2syhFrQeHibDRbE6M4f4J5dY',
+      callback: googleData => {
+        this.setState({
+          data: googleData
+        });
+      },
+      simpleSheet: true
+    });
+  }
+
+  calculateTotal = (quantity, price) => {
+    let total = 0;
+    for (let i = 0; i < this.state.data.length; i++) {
+      total += parseInt(this.state.data[i][quantity] * this.state.data[i][price]);
+    }
+
+    return total;
+  };
+
+  render() {
+    return (
+      <>
+        {
+          this.state.data.length ? (<Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Company</th>
+                <th>Quantity</th>
+                <th>Buying Price</th>
+                <th>Net Gain/Loss</th>
+              </tr>
+            </thead>
+            <tbody>
+              <>
+                {
+                  this.state.data.map((data, index) => {
+
+                    return (
+                      <tr key={index}>
+                        <td key={index}>{data['Company Name']}</td>
+                        <td>{data['Quantity']}</td>
+                        <td>{data['Buying Price']}</td>
+                        <td>{data['Quantity'] * Math.abs(data['Current Price'] - data['Buying Price'])} {data['Current Price'] > data['Buying Price'] ? 'Profit' : 'Loss'}</td>
+                      </tr>
+                    )
+                  })
+                }
+                {
+                  <tr>
+                    <td><b>Total Amount Invested</b></td>
+                    <td>{this.calculateTotal('Quantity', 'Buying Price')}</td>
+                  </tr>
+                }
+              </>
+            </tbody>
+          </Table>) : "Loading"
+        }
+      </>
+    );
+  }
 }
 
 export default App;
